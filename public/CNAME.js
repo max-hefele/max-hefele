99f4f36b8e1d8d566a3bf343a06870c7867cf226
max-hefele <mail@maxhefele.de>
2026-07-19 22:43:28 +0200

    Basispfad für Custom Domain angepasst

---
+++ public/CNAME.js
@@ -0,0 +1,1 @@
+maxhefele.de
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Wenn du eine Custom Domain nutzt → base: '/'
-  base: '/max-hefele/',
+  base: '/',
 })
