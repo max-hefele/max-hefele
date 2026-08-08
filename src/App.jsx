d509e8063e6dc8b5a28375884a871fe4d1cd9cba
Max-Hefele <maximilian.hefele85@web.de>
2026-08-08 10:14:33 +0200

    Beatport hinzugefügt

--- src/App.jsx
+++ src/App.jsx
@@ -153,7 +153,7 @@ const SOCIAL_LINKS = [
   { name: "YouTube", url: "https://www.youtube.com/@MaxHefele", icon: "YT" },
   { name: "Mixcloud", url: "https://www.mixcloud.com/MaxHefele/", icon: "MC" },
   { name: "Spotify", url: "#", icon: "SP", soon: true },
-  { name: "Beatport", url: "#", icon: "BP", soon: true }
+  { name: "Beatport", url: "https://www.beatport.com/artist/max-hefele/2396410", icon: "BP",  }
 ];
 
 const GOOGLE_DRIVE = {
